61ab7d2728a30497d013cfce43f7d48250f65786
JulioArgentinoRoca <lucagamasdemaio@gmail.com>
2026-04-08 12:06:25 -0300

    created react project in WEB directory

---
+++ WEB/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>hello und welcome 2 react</p>
+    </>
+  )
+}
+
+export default App
