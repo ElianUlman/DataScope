35c2e719d85f09e56f86b37a45ff85f61fa2b3da
Elian <49090707@est.ort.edu.ar>
2026-06-13 16:47:02 -0300

    correccion de imports

--- API/src/controllers/controller.js
+++ API/src/controllers/controller.js
@@ -3,7 +3,7 @@ export const initialPage = async (req, res) => {
     res.send("funciono");
 };
 
-import { tokenize } from "../utils/analizer.js";
+import { tokenize } from "../utils/nlpService.js";
 
 export const func4tests = async (req,res) =>{
     const result= await tokenize("hola, conta mis tokens", "gemini")
